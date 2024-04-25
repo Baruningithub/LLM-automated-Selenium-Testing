c978d6a8243e3c0b1e2635e861d47e0652a1aef2
barunh <Barunh@mindfiresolutions.com>
2024-04-25 12:18:12 +0530

    readme update

--- llama_test.py
+++ llama_test.py
@@ -3,21 +3,14 @@ from langchain_experimental.llms import ChatLlamaAPI
 
 llama = LlamaAPI("LL-VgYKjgRlpb0I7sFAUE5SPUipwyn6sINxJOmiOP5ACcUux2adLl5x8iDckNi5B8yf")
 
-# print(model.invoke("hello llama2").content)
 
-
-
-# from langchain_community.llms import GPT4All
 from langchain.chains import LLMChain
-
 from langchain_core.prompts import PromptTemplate
 from configs.read_config import Read_Configs,read_urls 
 from chat_templates import system_prompt, html_scrapper
 from logs.logger import logger
 from langchain_core.prompts import ChatPromptTemplate
 
-
-# model_path = ("/home/barunh/Documents/mpt-7b-chat-newbpe-q4_0.gguf")
 
 # fetching urls form config files
 page_list = ["admin_page", "data_entry"]

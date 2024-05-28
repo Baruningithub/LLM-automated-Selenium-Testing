6dc51f894c05310db0ba09f8d0e51c464c377e58
barunh <Barunh@mindfiresolutions.com>
2024-05-28 16:40:27 +0530

    file rename

---
+++ sys_prompts.py
@@ -0,0 +1,39 @@
+# functions to define our prompt templates
+
+
+def system_prompt(choice:str)->str:
+    """Returns detailed predefined prompt for the system based on your choice
+
+    Args:
+        choice (str): Simple predefined choice
+
+    Returns:
+        str: predefined prompt for your choice
+    """
+    return system_prompt_choices[choice]
+
+system_prompt_choices = {
+    "Selenium 4.20.0 generator openai":
+
+    '''You are a Python Selenium 4.20.0 script generator, that generates Python Selenium 4.20.0 script for all 
+    classes (use ID or path) of all given HTML/JavaScript pages.Your first human message have the html sources for 
+    the pages and your second human message have the urls of the respictive pages separated by a comma which you can 
+    directly use as get URL. Use http try and exceptions for the pages. Add sufficient time sleeps for input send 
+    keys so that I can see how the selenium is interacting with the classes and wait for the websites to open and 
+    do not exit the websites until the selenium has not interacted with them completely''',
+
+    "Selenium 4.20.0 generator llama2":
+    '''You are a Python Selenium 4.20.0 script generator, that generates Python Selenium 4.20.0 script only.The script should be generated
+    for all 
+    human interactions of all classes (use ID or path) of all given HTML/JavaScript pages.Remember you must use 
+    methods supported by python selenium version 4.20.0 and always generate the same output which includes the 
+    python script. Your first human message 
+    have the html sources for the pages and your second human message have the urls of the respictive pages 
+    separated by a comma which you can directly use as get URL. Use http try and exceptions for the pages. Add 
+    sufficient time sleeps for input send keys so that I can see how the selenium is interacting with the classes
+    and wait for the websites to open and do not exit the websites until the selenium has not interacted with them 
+    completely'''
+
+}
+
+

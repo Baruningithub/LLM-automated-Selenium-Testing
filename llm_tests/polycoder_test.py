9076edb4c742578a738d24a9a6a5373bd693d0f8
barunh <Barunh@mindfiresolutions.com>
2024-05-28 16:42:40 +0530

    updates

--- llm_tests/polycoder_test.py
+++ llm_tests/polycoder_test.py
@@ -1,10 +1,8 @@
 import transformers
 from transformers import AutoTokenizer, AutoModelForCausalLM
-import torch
+
 from packaging import version
 assert version.parse(transformers.__version__) >= version.parse("4.23.0")
 
-tokenizer = AutoTokenizer.from_pretrained("NinedayWang/PolyCoder-2.7B", torch_dtype=torch.float16,
- variant="fp16")
-model = AutoModelForCausalLM.from_pretrained("NinedayWang/PolyCoder-2.7B",  torch_dtype=torch.float16,
- variant="fp16")
+tokenizer = AutoTokenizer.from_pretrained("NinedayWang/PolyCoder-2.7B")
+model = AutoModelForCausalLM.from_pretrained("NinedayWang/PolyCoder-2.7B")
--- llm_tests/wizard_test.py
+++ llm_tests/wizard_test.py
@@ -1,29 +1,7 @@
-model="WizardLM/WizardCoder-33B-V1.1"
-temp=0.0
+
+model="/path/to/your/model"
+temp=0.2
 max_len=2048
-pred_num=1
-num_seqs_per_iter=1
+pred_num=200
+num_seqs_per_iter=2
 
-# output_path=preds/T${temp}_N${pred_num}_WizardCoder-33B-V1.1_Greedy_Decode
-
-# mkdir -p ${output_path}
-# echo 'Output path: '$output_path
-# echo 'Model to eval: '$model
-
-# # 164 problems, 21 per GPU if GPU=8
-# index=0
-# gpu_num=8
-# for ((i = 0; i < $gpu_num; i++)); do
-#   start_index=$((i * 21))
-#   end_index=$(((i + 1) * 21))
-
-#   gpu=$((i))
-#   echo 'Running process #' ${i} 'from' $start_index 'to' $end_index 'on GPU' ${gpu}
-#   ((index++))
-#   (
-#     CUDA_VISIBLE_DEVICES=$gpu python humaneval_gen.py --model ${model} \
-#       --start_index ${start_index} --end_index ${end_index} --temperature ${temp} \
-#       --num_seqs_per_iter ${num_seqs_per_iter} --N ${pred_num} --max_len ${max_len} --output_path ${output_path} --greedy_decode
-#   ) &
-#   if (($index % $gpu_num == 0)); then wait; fi
-# done

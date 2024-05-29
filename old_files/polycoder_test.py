97b3bb01363fa895e8aad987fe884e25e026c243
barunh <Barunh@mindfiresolutions.com>
2024-05-29 17:54:48 +0530

    lod llm tests

---
+++ old_files/polycoder_test.py
@@ -0,0 +1,8 @@
+import transformers
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+from packaging import version
+assert version.parse(transformers.__version__) >= version.parse("4.23.0")
+
+tokenizer = AutoTokenizer.from_pretrained("NinedayWang/PolyCoder-2.7B")
+model = AutoModelForCausalLM.from_pretrained("NinedayWang/PolyCoder-2.7B")
---
+++ old_files/wizard_test.py
@@ -0,0 +1,7 @@
+
+model="/path/to/your/model"
+temp=0.2
+max_len=2048
+pred_num=200
+num_seqs_per_iter=2
+

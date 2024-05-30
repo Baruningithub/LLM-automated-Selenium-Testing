b27264c0b1e489bc5904e4a6e0b90cbfe21fb79c
barunh <Barunh@mindfiresolutions.com>
2024-05-30 18:17:50 +0530

    repo changes

--- js/index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Validate the username and password (for simplicity, no actual authentication here)
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    window.location.href = 'data_entry.html'; // Redirect to another page after successful loginelse {
-    
-  });

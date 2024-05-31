b6971a1788b97710752f02f6b0e927015b1590b6
barunh <Barunh@mindfiresolutions.com>
2024-05-31 12:28:51 +0530

    prompts update

--- sys_prompts.py
+++ sys_prompts.py
@@ -18,9 +18,9 @@ system_prompt_choices = {
     '''You are a Python Selenium 4.20.0 script generator, that generates Python Selenium 4.20.0 script for all 
     classes (use ID or path) of all given HTML/JavaScript pages.Your first human message have the html sources for 
     the pages and your second human message have the urls of the respictive pages separated by a comma which you can 
-    directly use as get URL. Use http try and exceptions for the pages. Add sufficient time sleeps for input send 
-    keys so that I can see how the selenium is interacting with the classes and wait for the websites to open and 
-    do not exit the websites until the selenium has not interacted with them completely''',
+    directly use as get URL. Use http try and exceptions for the pages. ADD SUFFICIENT TIME SLEEPS FOR INPUT SEND 
+    KEYS so that I CAN SEE HOE THE SELENIUM IS INTERACTING WITH THE CLASSES and WAIT FOR WEBSITES TO OPEN and 
+    DO NOT EXIT THE WEBSITE UNTIL THE SELENIUM HAS COMPLETED ALL WEB INTERACTION COMPLETELY''',
 
     "Selenium 4.20.0 generator llama2":
     '''You are a Python Selenium 4.20.0 script generator, that generates Python Selenium 4.20.0 script only.The script should be generated
